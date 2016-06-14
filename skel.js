d523e9d4e32c270b75c91d51b536f027e55b4325
abdennour <mail@abdennoor.com>
2016-06-14 17:43:24 +0300

    best practices

---
+++ skel.js
@@ -0,0 +1,10 @@
+/**
+* Abdennour < https://cv.abdennoor.com>
+*/
+((g)=>{
+   ((rq,ps,echo,C,file,dir,mod,xp,delay,nDelay,job,nJob)=>{
+        //
+
+   })(g.require,g.process,g.console,g.Class,g.__filename,g.__dirname,g.module,g.exports,g.setTimeout,g.clearTimeout,g.setInterval,g.clearInterval)
+
+})(global)
